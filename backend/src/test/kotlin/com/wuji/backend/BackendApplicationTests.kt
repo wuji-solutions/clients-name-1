5b13264d0efef2bd19a75a44d3f84ebfa7dfa0aa
szaross <szymonszarek02@gmail.com>
2025-06-04 18:22:27 +0200

    spotless only check

--- backend/src/test/kotlin/com/wuji/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/wuji/backend/BackendApplicationTests.kt
@@ -6,7 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class BackendApplicationTests {
 
-  @Test fun contextLoads() {
+  @Test
+  fun contextLoads() {
     TODO("tests")
   }
 }
