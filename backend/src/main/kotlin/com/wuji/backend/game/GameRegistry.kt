9ec3ec7231228476ad2416dd63152bd588bf679b
szaross <szymonszarek02@gmail.com>
2025-07-21 18:36:18 +0200

    added exception

--- backend/src/main/kotlin/com/wuji/backend/game/GameRegistry.kt
+++ backend/src/main/kotlin/com/wuji/backend/game/GameRegistry.kt
@@ -4,6 +4,7 @@ import com.wuji.backend.game.common.AbstractGame
 import com.wuji.backend.game.common.GameConfig
 import com.wuji.backend.game.common.exception.GameNotCreatedYetException
 import com.wuji.backend.game.common.exception.IncorrectGameTypeException
+import com.wuji.backend.game.common.exception.ReportNotCreatedYetException
 import com.wuji.backend.player.state.PlayerDetails
 import com.wuji.backend.reports.common.GameReport
 import com.wuji.backend.reports.common.ReportRow
@@ -13,7 +14,7 @@ import org.springframework.stereotype.Component
 class GameRegistry {
     private var _game: AbstractGame<out PlayerDetails, out GameConfig>? = null
     private var _gameType: GameType? = null
-    var gameReport: GameReport<out ReportRow>? = null
+    private var _gameReport: GameReport<out ReportRow>? = null
 
     val game: AbstractGame<out PlayerDetails, out GameConfig>
         get() = _game ?: throw GameNotCreatedYetException()
@@ -21,13 +22,16 @@ class GameRegistry {
     val gameType: GameType
         get() = _gameType ?: throw GameNotCreatedYetException()
 
+    val gameReport: GameReport<out ReportRow>
+        get() = _gameReport ?: throw ReportNotCreatedYetException()
+
     fun register(
         game: AbstractGame<out PlayerDetails, out GameConfig>,
         gameReport: GameReport<out ReportRow>
     ) {
         this._game = game
         this._gameType = game.gameType
-        this.gameReport = gameReport
+        this._gameReport = gameReport
     }
 
     fun <T : AbstractGame<out PlayerDetails, out GameConfig>> getAs(
