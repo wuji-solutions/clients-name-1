baa69b040032b041eae81d82517c6fd7332374c7
Szymon Szarek <44586881+szaross@users.noreply.github.com>
2025-08-05 18:44:12 +0200

    simpler exceptions (#43)

--- backend/src/main/kotlin/com/wuji/backend/error/BasicErrorResponse.kt
+++ backend/src/main/kotlin/com/wuji/backend/error/BasicErrorResponse.kt
@@ -1,23 +1,9 @@
 package com.wuji.backend.error
 
 import java.time.LocalDateTime
-import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
-import org.springframework.http.ProblemDetail
-import org.springframework.web.ErrorResponse
 
 data class BasicErrorResponse(
     val status: Int,
     val message: String?,
     val timestamp: LocalDateTime = LocalDateTime.now(),
-) : ErrorResponse {
-    override fun getStatusCode(): HttpStatusCode = HttpStatus.valueOf(status)
-
-    override fun getBody(): ProblemDetail {
-        val detail =
-            ProblemDetail.forStatusAndDetail(
-                HttpStatus.valueOf(status), message)
-        detail.setProperty("timestamp", timestamp)
-        return detail
-    }
-}
+)
