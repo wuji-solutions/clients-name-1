af9c457690a651643d3c6b79e46bb0cecc7bbe22
Hubert Tułacz <htulacz@student.agh.edu.pl>
2025-08-06 00:32:10 +0200

    marshallers for configs in service

--- backend/src/main/kotlin/com/wuji/backend/config/creator/ConfigCreatorService.kt
+++ backend/src/main/kotlin/com/wuji/backend/config/creator/ConfigCreatorService.kt
@@ -1,14 +1,31 @@
 package com.wuji.backend.config.creator
 
+import com.wuji.backend.config.BoardConfig
+import com.wuji.backend.config.ExamConfig
+import com.wuji.backend.config.GameConfig
+import com.wuji.backend.config.QuizConfig
 import com.wuji.backend.config.dto.GameConfigDto
+import com.wuji.backend.game.GameType
+import jakarta.xml.bind.JAXBContext
+import jakarta.xml.bind.Marshaller
 import org.springframework.stereotype.Service
 
 @Service
-class ConfigCreatorService {
+class ConfigCreatorService(
+    val gameTypeToMarshaller: Map<GameType, Marshaller> = mapOf(
+        GameType.QUIZ to JAXBContext.newInstance(QuizConfig::class.java).createMarshaller(),
+        GameType.EXAM to JAXBContext.newInstance(ExamConfig::class.java).createMarshaller(),
+        GameType.BOARD to JAXBContext.newInstance(BoardConfig::class.java).createMarshaller(),
+    )
+) {
+    val configPath: String = "." // need to have a discussion where to store config files
+
     fun readConfig(): GameConfigDto {
         TODO("not yet implemented")
     }
-    fun createConfig() {
+    fun createConfig(
+        config: GameConfig,
+    ) {
         TODO("not yet implemented")
     }
 }
