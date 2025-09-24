baa70075eb9bc73650fd678b56bd3cd5e479d240
Szymon Szarek <44586881+szaross@users.noreply.github.com>
2025-09-24 10:04:09 +0200

    board game ranking (#66)
    * renking
    
    * docs ig
    
    * docs updated
    
    * xml parser
    
    * game creation, forgot last time xd
    
    * fix sse security
    
    * revert changes and apply proper ones
    
    * fix differences between docs and actual behaviour
    
    * xd
    
    * spotless
    
    * bug, ty @trechu

--- backend/src/main/kotlin/com/wuji/backend/player/state/BoardPlayer.kt
+++ backend/src/main/kotlin/com/wuji/backend/player/state/BoardPlayer.kt
@@ -13,6 +13,7 @@ class BoardPlayerDetails(
             DifficultyLevel.EASY
         },
     val askedQuestions: MutableList<Question> = mutableListOf(),
+    var points: Int = 0,
 ) : PlayerDetails()
 
 typealias BoardPlayer = Player<BoardPlayerDetails>
