8411173c2b4ffa0e42f3fcb6c4aac24f9ba78605
szaross <szymonszarek02@gmail.com>
2025-09-28 17:23:56 +0200

    send help

--- backend/src/main/kotlin/com/wuji/backend/util/ext/ListExt.kt
+++ backend/src/main/kotlin/com/wuji/backend/util/ext/ListExt.kt
@@ -2,10 +2,6 @@ package com.wuji.backend.util.ext
 
 import com.wuji.backend.question.common.Question
 
-fun <T> List<T>.getOrThrow(id: Int, exceptionProvider: () -> Throwable): T {
-    return getOrNull(id) ?: throw exceptionProvider()
-}
-
 fun List<Question>.getCategories(): List<String> {
     return this.map { it.category }.distinct()
 }
