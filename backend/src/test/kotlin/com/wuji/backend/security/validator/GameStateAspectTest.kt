4d8553310d28d9bb911e04b8e9f373ae952f91b8
szaross <szymonszarek02@gmail.com>
2025-10-01 21:20:40 +0200

    more tests

--- backend/src/test/kotlin/com/wuji/backend/security/validator/GameStateAspectTest.kt
+++ backend/src/test/kotlin/com/wuji/backend/security/validator/GameStateAspectTest.kt
@@ -2,6 +2,8 @@ package com.wuji.backend.security.validator
 
 import com.wuji.backend.game.GameType
 import com.wuji.backend.game.common.GameServiceDelegate
+import com.wuji.backend.game.common.GameState
+import com.wuji.backend.game.common.exception.GameInIncorrectStateException
 import com.wuji.backend.security.validator.exception.InvalidGameTypeException
 import io.mockk.every
 import io.mockk.mockk
@@ -9,6 +11,8 @@ import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.Signature
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 
 class GameStateAspectTest {
 
@@ -17,6 +21,12 @@ class GameStateAspectTest {
     private val aspect = GameStateAspect(mockGameServiceDelegate)
 
     @RequiresGame(GameType.BOARD) class BoardGameController
+
+    @GameCreated class CreatedController
+
+    @GameRunning class RunningController
+
+    @GamePaused class PausedController
 
     private fun mockJoinPoint(target: Any): JoinPoint {
         val jp = mockk<JoinPoint>()
@@ -42,5 +52,49 @@ class GameStateAspectTest {
         assertThrows(InvalidGameTypeException::class.java) {
             aspect.checkGameType(joinPoint)
         }
+    }
+
+    @Test
+    fun `checkCreated succeeds when game state is CREATED`() {
+        every { mockGameServiceDelegate.getGameState() } returns
+            GameState.CREATED
+
+        assertDoesNotThrow { aspect.checkCreated() }
+    }
+
+    @Test
+    fun `checkCreated fails when game state is not CREATED`() {
+        every { mockGameServiceDelegate.getGameState() } returns
+            GameState.RUNNING
+
+        assertThrows<GameInIncorrectStateException> { aspect.checkCreated() }
+    }
+
+    @Test
+    fun `checkRunning succeeds when game state is RUNNING`() {
+        every { mockGameServiceDelegate.getGameState() } returns
+            GameState.RUNNING
+        assertDoesNotThrow { aspect.checkRunning() }
+    }
+
+    @Test
+    fun `checkRunning fails when game state is not RUNNING`() {
+        every { mockGameServiceDelegate.getGameState() } returns
+            GameState.PAUSED
+        assertThrows<GameInIncorrectStateException> { aspect.checkRunning() }
+    }
+
+    @Test
+    fun `checkPaused succeeds when game state is PAUSED`() {
+        every { mockGameServiceDelegate.getGameState() } returns
+            GameState.PAUSED
+        assertDoesNotThrow { aspect.checkPaused() }
+    }
+
+    @Test
+    fun `checkPaused fails when game state is not PAUSED`() {
+        every { mockGameServiceDelegate.getGameState() } returns
+            GameState.RUNNING
+        assertThrows<GameInIncorrectStateException> { aspect.checkPaused() }
     }
 }
