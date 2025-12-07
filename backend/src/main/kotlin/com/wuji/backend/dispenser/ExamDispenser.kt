1e607f381084fc213109bc36877fb243b861172a
Szymon Szarek <44586881+szaross@users.noreply.github.com>
2025-12-07 20:49:25 +0100

    [FIX] Fix completing exams where there are no additional questions (#139)

--- backend/src/main/kotlin/com/wuji/backend/dispenser/ExamDispenser.kt
+++ backend/src/main/kotlin/com/wuji/backend/dispenser/ExamDispenser.kt
@@ -4,9 +4,34 @@ import com.wuji.backend.config.DifficultyLevel
 import com.wuji.backend.player.state.Player
 import com.wuji.backend.player.state.PlayerDetails
 import com.wuji.backend.player.state.PlayerIndex
+import com.wuji.backend.question.common.Answer
 import com.wuji.backend.question.common.Question
+import com.wuji.backend.question.common.QuestionType
+import com.wuji.backend.question.common.TextFormat
 
 class ExamDispenser {
+    companion object {
+        private val additionalFeedbackQuestion =
+            Question(
+                id = 999,
+                category = "",
+                type = QuestionType.TEXT,
+                text = "Jak ci się podobał sprawdzian",
+                textFormat = TextFormat.HTML,
+                answers =
+                    listOf(
+                        Answer.create("Bardzo mi się podobał"),
+                        Answer.create("Podobał mi się"),
+                        Answer.create("Było ok"),
+                        Answer.create("Nie podobał mi się"),
+                        Answer.create("Wcale mi się nie podobał")),
+                correctAnswerIds = setOf(),
+                difficultyLevel = DifficultyLevel.EASY,
+                images = null,
+                tags = listOf(),
+            )
+    }
+
     val dispensers: MutableMap<PlayerIndex, PlayerDispenser> = mutableMapOf()
 
     fun initialize(
@@ -15,33 +40,48 @@ class ExamDispenser {
         requiredQuestionCount: Int,
         randomizeQuestions: Boolean,
         enforceDifficultyBalance: Boolean,
+        withAdditionalFeedbackQuestion: Boolean = true,
     ) {
         for (player in players) {
             val questions =
                 shuffleQuestions(
                     questions, randomizeQuestions, enforceDifficultyBalance)
+            val additionalQuestions =
+                questions.subList(requiredQuestionCount, questions.size).let {
+                    if (withAdditionalFeedbackQuestion)
+                        it + additionalFeedbackQuestion
+                    else it
+                }
             dispensers.put(
                 player.index,
                 PlayerDispenser(
                     questions.subList(0, requiredQuestionCount),
-                    questions.subList(requiredQuestionCount, questions.size)))
+                    additionalQuestions,
+                ))
         }
     }
 
     fun initialize(
         players: Set<Player<out PlayerDetails>>,
         questions: List<Question>,
-        selectedQuestionIds: Set<Int>
+        selectedQuestionIds: Set<Int>,
+        withAdditionalFeedbackQuestion: Boolean = true
     ) {
         val baseQuestions =
             questions.filter { selectedQuestionIds.contains(it.id) }
+        val additionalQuestions =
+            questions
+                .filter { !selectedQuestionIds.contains(it.id) }
+                .let {
+                    if (withAdditionalFeedbackQuestion)
+                        it + additionalFeedbackQuestion
+                    else it
+                }
 
         for (player in players) {
             dispensers.put(
                 player.index,
-                PlayerDispenser(
-                    baseQuestions,
-                    questions.filter { !selectedQuestionIds.contains(it.id) }))
+                PlayerDispenser(baseQuestions, additionalQuestions))
         }
     }
 
